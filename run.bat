522e74f85c3f678de2e961815764451f46587a2a
it <it@csjob.ru>
2026-01-30 01:20:55 +0300

    Fix CRD annotation size limit issue: Add ignoreDifferences for CustomResourceDefinitions

--- run.bat
+++ run.bat
@@ -1,5 +1,6 @@
 @echo off
-REM Alertmanager removed - using kube-prometheus-stack integrated version
 kubectl apply -f argocd-kube-prometheus-stack.yaml -n argocd
 kubectl apply -f argocd-loki.yaml -n argocd
 kubectl apply -f argocd-prometheus-operator-crds.yaml -n argocd
+kubectl get secret kube-prometheus-stack-grafana -n monitoring -o jsonpath="{.data.admin-password}" | base64 -d
+kubectl port-forward svc/kube-prometheus-stack-grafana -n monitoring 3000:80
