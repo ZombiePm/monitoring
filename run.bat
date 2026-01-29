f2fff4f303359d3426d78e1d8dfcb9c9f4931103
it <it@csjob.ru>
2026-01-30 00:58:35 +0300

    Remove duplicate Alertmanager configuration to prevent deployment conflicts

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 @echo off
-kubectl apply -f argocd-alertmanager.yaml -n argocd
+REM Alertmanager removed - using kube-prometheus-stack integrated version
 kubectl apply -f argocd-kube-prometheus-stack.yaml -n argocd
 kubectl apply -f argocd-loki.yaml -n argocd
 kubectl apply -f argocd-prometheus-operator-crds.yaml -n argocd
