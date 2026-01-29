23ca37f2aa47b62ed59773e599b156ce3ec7566f
it <it@csjob.ru>
2026-01-30 01:31:52 +0300

    Restore complete Promtail configuration for proper log shipping to Loki

--- run.bat
+++ run.bat
@@ -2,5 +2,5 @@
 kubectl apply -f argocd-kube-prometheus-stack.yaml -n argocd
 kubectl apply -f argocd-loki.yaml -n argocd
 kubectl apply -f argocd-prometheus-operator-crds.yaml -n argocd
-kubectl get secret kube-prometheus-stack-grafana -n monitoring -o jsonpath="{.data.admin-password}" | base64 -d
-kubectl port-forward svc/kube-prometheus-stack-grafana -n monitoring 3000:80
+#kubectl get secret kube-prometheus-stack-grafana -n monitoring -o jsonpath="{.data.admin-password}" | base64 -d
+#kubectl port-forward svc/kube-prometheus-stack-grafana -n monitoring 3000:80
