d45066c9541a199b9261553fbd08dd82a1cf34d4
it <it@csjob.ru>
2026-01-29 22:59:29 +0300

    Initial commit: Clean monitoring stack with ArgoCD

---
+++ run.bat
@@ -0,0 +1,5 @@
+@echo off
+kubectl apply -f argocd-alertmanager.yaml -n argocd
+kubectl apply -f argocd-kube-prometheus-stack.yaml -n argocd
+kubectl apply -f argocd-loki.yaml -n argocd
+kubectl apply -f argocd-prometheus.yaml -n argocd
