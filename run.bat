bb14f93efac81d59826f0e4fee503b12ba8d8355
it <it@csjob.ru>
2026-01-30 00:32:53 +0300

    Clean up monitoring stack: Remove duplicate Prometheus deployment and fix configuration conflicts

--- run.bat
+++ run.bat
@@ -2,4 +2,4 @@
 kubectl apply -f argocd-alertmanager.yaml -n argocd
 kubectl apply -f argocd-kube-prometheus-stack.yaml -n argocd
 kubectl apply -f argocd-loki.yaml -n argocd
-kubectl apply -f argocd-prometheus.yaml -n argocd
+kubectl apply -f argocd-prometheus-operator-crds.yaml -n argocd
